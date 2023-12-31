a27dce0503178de1e877a02f6769003b98c98aa6
Vignesh S <vign.71772118152@gct.ac.in>
2023-12-31 21:14:01 +0530

    small bug fixed

--- src/sections/CartContainer.jsx
+++ src/sections/CartContainer.jsx
@@ -21,66 +21,68 @@ const CartContainer = () => {
     setIsOrderConfirmed(false);
   };
   return (
-    <div
-      data-aos="slide-left"
-      data-aos-duration="600"
-      className="fixed top-0 right-0 z-[100] w-full md:w-[375px] h-screen bg-slate-50 flex flex-col overflow-y-scroll snap-y "
-    >
-      <div className="w-full flex items-center justify-between p-4 cursor-pointer">
-        <MdOutlineKeyboardBackspace
-          className="text-secondary text-3xl"
-          onClick={() => {
-            dispatch(hideCart());
-          }}
-        />
-        <p className="text-secondary text-lg font-semibold font-poppins">
-          Cart
-        </p>
-
-        <div
-          className="flex font-poppins items-center gap-2 p-1 px-2 my-2 bg-dark-coffee rounded-md hover:shadow-md  cursor-pointer text-secondary text-base"
-          onClick={() => {
-            dispatch(clearCart());
-          }}
-        >
-          Clear <RiRefreshFill />
-        </div>
-      </div>
-      {amount > 0 ? (
-        <>
-          <div className="w-full flex flex-col px-3">
-            {cartItems.map((item) => {
-              return <CartItem key={item.id} {...item} />;
-            })}
-          </div>
-          <div className="fixed w-full md:w-[350px] bottom-0 bg-coffee  pt-10 pb-10 flex items-center flex-col">
-            <h4 className="capitalize flex justify-between items-center font-poppins font-semibold text-secondary  px-4 md:w-full  mb-4">
-              total{" "}
-              <span className="max-sm:ml-52">
-                <FaRupeeSign className="inline pb-1 " />
-                {total.toFixed(2)}
-              </span>
-            </h4>
-            <div onClick={handleCheckOut}>
-              <Button checkOut={true}>Check Out</Button>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="flex justify-center items-center h-[300px] flex-col gap-6 pt-32">
-          <img
-            src={emptyCartImg}
-            className="w-[300px] h-[300px] object-contain"
+    <>
+      <div
+        data-aos="slide-left"
+        data-aos-duration="600"
+        className="fixed top-0 right-0 z-[100] w-full md:w-[375px] h-screen bg-slate-50 flex flex-col overflow-y-scroll snap-y "
+      >
+        <div className="w-full flex items-center justify-between p-4 cursor-pointer">
+          <MdOutlineKeyboardBackspace
+            className="text-secondary text-3xl"
+            onClick={() => {
+              dispatch(hideCart());
+            }}
           />
-          <h2 className="font-poppins font-semibold text-xl tracking-wide">
-            Grab a coffee
-          </h2>
+          <p className="text-secondary text-lg font-semibold font-poppins">
+            Cart
+          </p>
+
+          <div
+            className="flex font-poppins items-center gap-2 p-1 px-2 my-2 bg-dark-coffee rounded-md hover:shadow-md  cursor-pointer text-secondary text-base"
+            onClick={() => {
+              dispatch(clearCart());
+            }}
+          >
+            Clear <RiRefreshFill />
+          </div>
         </div>
-      )}
+        {amount > 0 ? (
+          <>
+            <div className="w-full flex flex-col px-3">
+              {cartItems.map((item) => {
+                return <CartItem key={item.id} {...item} />;
+              })}
+            </div>
+            <div className="fixed w-full md:w-[350px] bottom-0 bg-coffee  pt-10 pb-10 flex items-center flex-col">
+              <h4 className="capitalize flex justify-between items-center font-poppins font-semibold text-secondary  px-4 md:w-full  mb-4">
+                total{" "}
+                <span className="max-sm:ml-52">
+                  <FaRupeeSign className="inline pb-1 " />
+                  {total.toFixed(2)}
+                </span>
+              </h4>
+              <div onClick={handleCheckOut}>
+                <Button checkOut={true}>Check Out</Button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center items-center h-[300px] flex-col gap-6 pt-32">
+            <img
+              src={emptyCartImg}
+              className="w-[300px] h-[300px] object-contain"
+            />
+            <h2 className="font-poppins font-semibold text-xl tracking-wide">
+              Grab a coffee
+            </h2>
+          </div>
+        )}
+      </div>
       {isOrderConfirmed && (
         <OrderConfirmationModal onClose={handleCloseModal} />
       )}
-    </div>
+    </>
   );
 };
 
