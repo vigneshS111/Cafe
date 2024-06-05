84a86a33041e445ac86fc5895cba8fa43f18dc18
vigneshviky01 <vign.71772118152@gct.ac.in>
2024-06-05 17:46:10 +0530

    bug in notFound page fixed

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <div className="h-screen flex flex-col items-center gap-6 py-14">
       <h1 className="text-[72px]">404</h1>
       <h2 className="text-[36px]">Page Not Found</h2>
-      <p className="text-[18px]">
+      <p className="text-[18px] px-6">
         Sorry, the page you are looking for does not exist.
       </p>
       <Link to="/" style={{ fontSize: "18px", color: "blue" }}>
